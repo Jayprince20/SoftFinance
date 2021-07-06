fc62c507925daea293b5c3e3deb5e3ddd722c9c6
unknown <tebahprincewill6@gmail.com>
2021-07-06 18:54:33 +0100

    the ui

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -22,30 +22,138 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-            height: 450,
-            color: Colors.blue,
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff36cbf2),
+                Color(0xff34d8f3),
+                Color(0xff3cbff0),
+              ],
+              begin: FractionalOffset.topLeft,
+              end: FractionalOffset.bottomRight,
+            ),
+          ),
+          height: 400,
+          //color: Color(0xff36cbf2),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(25.0),
+                            child: CircleAvatar(
+                              maxRadius: 25,
+                              backgroundImage:
+                                  AssetImage("assets/icons/twitch.jpg"),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("Hello",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20)),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text("One Achmad",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20))
+                            ],
+                          ),
+                        ],
+                      ))
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Stack(children: [
+                      Icon(
+                        Icons.notifications_outlined,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      Positioned(
+                          right: 10,
+                          bottom: 22,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.red,
+                            maxRadius: 5,
+                          ))
+                    ]),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text("Balance",
+                  style: TextStyle(fontSize: 20, color: Colors.white)),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "\$26,520",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
                   children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("Icons"),
-                        )
-                      ],
+                    Card(
+                      name: Icons.send,
+                      text: "Send",
                     ),
-                    Text("Hello")
+                    Card(
+                      name: Icons.wallet_giftcard,
+                      text: "Top Up",
+                    ),
+                    Card(
+                      name: Icons.payment,
+                      text: "Pay",
+                    ),
+                    Card(
+                      name: Icons.dashboard,
+                      text: "More",
+                    ),
                   ],
                 ),
-                SizedBox(height: 60,),
-                Text("Balance", style: TextStyle(fontSize:20)),
-                SizedBox(height: 30,),
-                Text("26, 520", style: TextStyle(fontSize: 30),),
-              ],
-            )),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(90.0),
+                      topRight: Radius.circular(90.0),
+                    ),
+                  ),
+                  child: Row(  
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Last Transaction",
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "See all",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: DotNavigationBar(
         currentIndex: _SelectedTab.values.indexOf(_selectedTab),
@@ -77,6 +185,41 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({
+    this.name,
+    this.text,
+    Key key,
+  }) : super(key: key);
+  final String text;
+  final IconData name;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Color(0xff36cbf2).withOpacity(0.7),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Icon(
+                name,
+                color: Colors.white,
+              )),
+        ),
+        Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        )
+      ],
     );
   }
 }
